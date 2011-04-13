1c32a81f629df78f9cc7705463c03bcea084f929
Christopher Loessl <cloessl@x-berg.de>
2011-04-13 12:58:06 +0200

    Lists in logcat all apps + fires up googlemaps
    on btn click

--- src/de/tubs/kiosk/android/Kiosk.java
+++ src/de/tubs/kiosk/android/Kiosk.java
@@ -1,13 +1,56 @@
 package de.tubs.kiosk.android;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Kiosk extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private Button mTest = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+
+		mTest = (Button) findViewById(R.id.btnTest);		
+
+		if ( mTest != null)
+			mTest.setOnClickListener( new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					Log.w("test", "test22");
+
+					Intent  googlemaps = null;
+
+					final PackageManager pm = getPackageManager();
+					if (pm == null)
+						return;
+					List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+					for (ApplicationInfo packageInfo : packages) {
+
+						Log.d("nu", "Installed package :" + packageInfo.packageName);
+						Log.d("nu",	"Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+
+						if ( packageInfo.packageName.contains("android.apps.maps"))
+							googlemaps = pm.getLaunchIntentForPackage(packageInfo.packageName);
+
+					}
+
+					startActivity(googlemaps);
+
+				}					
+			});
+	}
 }
