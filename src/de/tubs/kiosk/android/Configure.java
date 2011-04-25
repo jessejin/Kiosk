8ca9b353fa4ab13641b62fbb1be07e974bec9189
Christopher Loessl <cloessl@x-berg.de>
2011-04-25 11:04:15 +0200

    Prefs + Menu working
    Added 2 new Activities + Preferences + Menu
        - Preferences Activiti
        - Configure Activiti (checks pw)
    
    Now you need the root pw to make any changes to the preferences

---
+++ src/de/tubs/kiosk/android/Configure.java
@@ -0,0 +1,60 @@
+package de.tubs.kiosk.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Configure extends Activity implements OnClickListener {
+	
+	SharedPreferences mPrefs = null;
+	Button mBtnOK = null;
+	TextView mtextPassword = null;
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.configure);
+		
+		mBtnOK = (Button) findViewById(R.id.checkrootpwok);
+		mtextPassword = (TextView) findViewById(R.id.rootpw);
+
+		if ( mBtnOK != null)
+			mBtnOK.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		String textUserPW = mtextPassword.getText().toString();
+		String prefsPW = mPrefs.getString("passwd", "uppsala");
+		
+		if ( textUserPW.equals( prefsPW) ) {
+			startActivity(new Intent(this,Preferences.class));			
+		} else
+			Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		mPrefs = getSharedPreferences("de.tubs.kiosk.android_preferences", MODE_PRIVATE);
+		
+		TextView name = (TextView) findViewById(R.id.textView1);
+		name.setText("[DEBUG] current pw: <<" + mPrefs.getString("passwd","uppsala") + ">>");
+	}
+
+}
--- src/de/tubs/kiosk/android/Kiosk.java
+++ src/de/tubs/kiosk/android/Kiosk.java
@@ -8,6 +8,9 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,6 +18,35 @@ import android.widget.Button;
 public class Kiosk extends Activity {
 
 	private Button mTest = null;
+	
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.root_menu, menu);
+//        return true;
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.configure:
+	    	startActivity(new Intent(this,Configure.class));
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
